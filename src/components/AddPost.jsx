41287c2888034dfecf8694bcbf2b4c9e4e549481
Zeynp62 <zainab.fadhel.alaali62@gmail.com>
2024-12-29 21:33:38 +0300

    added axios to package

---
+++ src/components/AddPost.jsx
@@ -0,0 +1,9 @@
+import { useNavigate } from 'react-router-dom'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+const AddPost = () => {
+  let navigate = useNavigate()
+  const initial = {}
+}
+return
+export default AddPost
